0c27c12409bcaa3a8be5a50139af2392d045e604
sepengsu <na06219@naver.com>
2024-01-16 11:38:49 +0900

    ee

---
+++ customreward/deprlpaper/decay.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+# Set non-random initial muscle activations
+import sys
+# Add sconepy folders to path
+if sys.platform.startswith("win"):
+    sys.path.append("C:/Program Files/SCONE/bin")
+elif sys.platform.startswith("linux"):
+    sys.path.append("/opt/scone/lib")
+elif sys.platform.startswith('darwin'):
+    sys.path.append("/Applications/SCONE.app/Contents/MacOS/lib")
+import sconepy
+
+def alpha_t(cur_reward,theta,labmda):
+    
