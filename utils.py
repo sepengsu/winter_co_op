7801d67e4dec2b7bbec855936ee7fdf54e56fd09
seojaewon <111292354+sepengsu@users.noreply.github.com>
2024-01-17 11:40:25 +0900

    local

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import datetime
 import time
 from deprl import main
-
+import os
 def run_training(config):
     print(datetime.datetime.now().strftime("%Y년 %m월 %d일 %H:%M:%S"))
     for i in range(50):
@@ -25,3 +25,23 @@ def run_training(config):
         print(f"Iteration {i+1}, Duration: {int(minutes)}분 {int(seconds)}초")
         print(f"End Time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
         print("-" * 30)
+
+def get_directory_path():
+    dir_path = [
+        r"C:\Users\PC\Documents\SCONE\results",
+        r"C:\Users\na062\Documents\SCONE\results"
+    ]
+    
+    for path in dir_path:
+        if os.path.exists(path) or os.path.isdir(path):
+            return path
+    while True:
+        name = input("사용자 이름: ")
+        dir_path = r"C:\Users\{}\Documents\SCONE\results".format(name)
+        if os.path.exists(dir_path) or os.path.isdir(dir_path):
+            return dir_path
+        else:
+            print("경로를 찾을 수 없습니다.")
+            print("경로를 다시 입력하세요.")
+            continue
+
