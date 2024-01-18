f77a2f90dc948a1127c94c74baf72a56c9b5e50a
seojaewon <111292354+sepengsu@users.noreply.github.com>
2024-01-18 11:33:42 +0900

    GOGO

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,5 @@
-from . import model_analysis
+
 from .train_utils import run_training, get_directory_path, configmake
+from .inference_utils import inference
+from .model_analysis import printing_var, direxcept, namedict
+
