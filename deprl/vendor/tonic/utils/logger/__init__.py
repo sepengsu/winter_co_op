78389dce4b544420eae45b06dd2fb3e4124aacb9
seojaewon <111292354+sepengsu@users.noreply.github.com>
2024-01-19 22:48:36 +0900

    g

--- deprl/vendor/tonic/utils/logger/__init__.py
+++ deprl/vendor/tonic/utils/logger/__init__.py
@@ -3,4 +3,7 @@ from myutils import logger
 
 functions = [name for name, value in inspect.getmembers(logger) 
              if inspect.isfunction(value) and not inspect.ismodule(value) and not inspect.isclass(value)]
+
+globals().update({name: getattr(logger, name) for name in functions})
+
 __all__ = functions
