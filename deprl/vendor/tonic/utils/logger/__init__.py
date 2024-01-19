4c331bc3e51bddf5e6af27c9591dd640176fea19
seojaewon <111292354+sepengsu@users.noreply.github.com>
2024-01-19 22:32:02 +0900

    g

--- deprl/vendor/tonic/utils/logger/__init__.py
+++ deprl/vendor/tonic/utils/logger/__init__.py
@@ -1,3 +1,6 @@
+import inspect
 from myutils import logger
 
-__all__ = [name for name in dir(logger) if not (name.startswith("_") and name=='MyLogger')]
+functions = [name for name, value in inspect.getmembers(logger) 
+             if inspect.isfunction(value) and not inspect.ismodule(value) and not inspect.isclass(value)]
+__all__ = functions
