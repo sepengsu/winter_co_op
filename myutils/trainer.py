b7f2caa563ec3108e759349958cdd1fb40d329c7
seojaewon <111292354+sepengsu@users.noreply.github.com>
2024-01-20 13:06:34 +0900

    0

--- myutils/trainer.py
+++ myutils/trainer.py
@@ -138,6 +138,7 @@ class MyTrainer(Trainer):
                     # save time iteration dict
                     self.save_time(save_path, epochs, episodes)
                     steps_since_save = self.steps % self.save_steps
+                    logger.log(f"Saved a checkpoint")
 
                 if stop_training:
                     self.close_mp_envs()
