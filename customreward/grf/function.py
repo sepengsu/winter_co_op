ac86cfee1f2a4da75d657c25d262534127339f52
sepengsu <na06219@naver.com>
2024-01-22 23:38:15 +0900

    GO

--- customreward/grf/function.py
+++ customreward/grf/function.py
@@ -1,12 +1,16 @@
 
 
 DEFAULT_WEIGHTS ={
+    'grfdelta': 1,
+    'grf' : 0
 }
 def totalreward(model,head_hody,grf,prev_excs,weight = DEFAULT_WEIGHTS,**kwargs):
     '''
     여기서는 크게 3가지로 나눠서 계산한다.
     '''
-    types = kwargs.get('grf_types',None)
-    
-
+    typesdelta = kwargs.get('grf_types',None)
+    grfdelta = kwargs.get('grf_grfdelta',None)
+    Grf = kwargs.get('grf_grf',None)
+    if grfdelta:
+        DEFAULT_WEIGHTS['grfdetla']
     pass
