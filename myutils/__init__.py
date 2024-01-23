ba7654ed2f2b453f613b38dede814ce82c91e021
seojaewon <111292354+sepengsu@users.noreply.github.com>
2024-01-23 16:55:17 +0900

    240121111

--- myutils/__init__.py
+++ myutils/__init__.py
@@ -1,6 +1,6 @@
-from .config_utils import get_directory_path, configmake, make_weight_dict, make_type_dict
+from .config_utils import get_directory_path, configmake, make_weight_dict, make_type_dict, make_env
 from .inference_utils import inference, plot_3d, plot_2d
 from .analysis import printing_var, direxcept, namedict
 
-__all__ = [get_directory_path, configmake, inference, plot_3d, plot_2d, printing_var, direxcept, namedict,make_weight_dict,make_type_dict]
+__all__ = [get_directory_path, configmake, inference, plot_3d, plot_2d, printing_var, direxcept, namedict,make_weight_dict,make_type_dict,make_env]
 
