57ef0d684752402f4b0919f2a537a01da9dff577
sepengsu <na06219@naver.com>
2024-01-29 01:52:45 +0900

    변경

--- sconegym/__init__.py
+++ sconegym/__init__.py
@@ -49,6 +49,28 @@ register(id="sconewalk_noLateral-v1",
              }
          }
         )
+
+register(id="sconewalk_noLateral-v2",
+         entry_point="sconegym.gaitgym:GaitGym",
+         kwargs={
+             'model_file': curr_dir + '/data-v1/H1622_noLateral2.scone',
+             'obs_type': '3D',
+             'left_leg_idxs': [6, 7, 8, 9, 10],
+             'right_leg_idxs': [11, 12, 13, 14, 15],
+             'clip_actions': True,
+             'run': False,
+             'target_vel': 1.2,
+             'leg_switch': True,
+             'rew_keys':{
+                "vel_coeff": 10,
+                "grf_coeff": -0.07281,
+                "joint_limit_coeff": -0.1307,
+                "smooth_coeff": -0.097,
+                "nmuscle_coeff": -1.57929,
+                "self_contact_coeff": 0.0,
+             }
+         }
+        )
 # Walk Environments
 register(id="sconewalk_h0918-v1",
          entry_point="sconegym.gaitgym:GaitGym",
